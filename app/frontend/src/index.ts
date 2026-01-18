c2c32069686e0bed694167a28d4af32cc6b2d2ec
Nicolas <ponchon.nicolas@gmail.com>
2026-01-18 14:57:15 +0100

    Removed the app since the images are now hosted on GHCR

--- app/frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import './styles/tailwind.css';
-import { startRouter, navigate } from './utils/router';
-
-const app = document.getElementById('app');
-if (!app) throw new Error('App container not found');
-
-(window as any).navigate = navigate;
-
-startRouter(app);
